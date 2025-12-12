72b694cb7593bbf0bab16d8ef646e2aeef829303
David Courtey <david_courtey@yahoo.com>
2025-12-12 13:37:07 +0100

    chore: remove Laravel Octane dependencies and update Docker initialization
    - Remove `config/octane.php` and drop Laravel Octane package from `composer.json`.
    - Adjust `init.sh` to replace Octane's frankenphp server setup with Scribe generation and default entrypoint.
    - Update `.gitignore` to include Scribe files and remove unused Octane-related entries.
    - Change default port in `docker-compose.yml` from `8001` to `8000`.
    - Upgrade various dependencies in `composer.lock`.

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -3,9 +3,8 @@ set -e
 
 if [ "$APP_ENV" = "production" ]; then
     php artisan optimize
-    php artisan migrate --force
-    php artisan octane:frankenphp --host=0.0.0.0 --port=8000
-else
-    php artisan migrate --force
-    docker-php-entrypoint --config /etc/frankenphp/Caddyfile --adapter caddyfile
 fi
+
+php artisan migrate --force
+php artisan scribe:generate
+docker-php-entrypoint --config /etc/frankenphp/Caddyfile --adapter caddyfile
