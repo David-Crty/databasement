388673dc0846550a675d0a3454d52035f51bc6ba
David Courtey <david_courtey@yahoo.com>
2026-04-23 13:35:48 +0200

    fix: update database server index layout for improved label truncation and accessibility

--- resources/views/livewire/database-server/connection-status.blade.php
+++ resources/views/livewire/database-server/connection-status.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if($loading)
-        <span role="button" tabindex="0" title="{{ __('Checking connection') }}" class="block size-2.5 rounded-full bg-base-content/20 ring-2 ring-base-100 animate-ping cursor-pointer"></span>
+        <span title="{{ __('Checking connection') }}" class="block size-2.5 rounded-full bg-base-content/20 ring-2 ring-base-100 animate-ping"></span>
     @else
-        <span role="button" tabindex="0" title="{{ $message }}" class="block size-2.5 rounded-full ring-2 ring-base-100 cursor-pointer {{ $success ? 'bg-success' : 'bg-error' }}"></span>
+        <span aria-label="{{ $message }}" title="{{ $message }}" class="block size-2.5 rounded-full ring-2 ring-base-100 {{ $success ? 'bg-success' : 'bg-error' }}"></span>
     @endif
 </div>
